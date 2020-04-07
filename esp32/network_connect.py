fada8afcc31df808d7031f6a85edd87bc3bd58b7
Isaiah Grace <igrace@purdue.edu>
2020-04-07 15:32:44 -0400

    network_connect is now a little more reliable, can handle incorrect entries in the saved networks database

--- esp32/network_connect.py
+++ esp32/network_connect.py
@@ -41,7 +41,6 @@ def scan_networks():
 	
 	if len(networks) == 0:
 		print("ERROR! No WiFi networks found")
-		# TODO: Make LEDs flash red and yellow
 		return False
 		
 	# Search for known networks, because we expect to be in range
@@ -63,40 +62,62 @@ def connect():
 	if wlan == None:
 		print("ERROR, initilize wlan before attempting to connect")
 		return False
-	
-	if network_name == None or network_passwd == None:
-		print("ERROR! network_name and network_passwd not set")
-		return False
 
 	if wlan.isconnected():
 		print("INFO, wlan already connected")
 		return True
+
+	if network_name == None or network_passwd == None:
+		print("ERROR! network_name and network_passwd not set")
+		return False
+
 
 	wlan.connect(network_name,network_passwd)
 
 	timeout = 0
 	while not wlan.isconnected():
 		if timeout > 10:
-			raise Exception
+			wlan.disconnect()
+			print("Timed out waiting to connect")
+			if network_name in saved_networks_dict:
+				print("would you like to delete the saved network entry?")
+				print("network_name:",network_name)
+				print("network_passwd:",network_passwd)
+				delete_entry = input("[Y/n]")
+				if delete_entry == "y" or delete_entry == "Y":
+					del saved_networks_dict[network_name]
+					with open("known_networks","w") as f:
+						ujson.dump(saved_networks_dict,f)
+
+			network_name = None
+			network_passwd = None
+				
+			return False
+		
 		print("Waiting for wlan to connect... " ,timeout)
 		timeout = timeout + 1
 		sleep_ms(3000)
 
 	print("Connceted to WiFi, getting time from ntp")
 	ntptime.settime()
+	return True
 
 def prompt_user_network():
+	global wlan
 	global network_name
 	global network_passwd
 	global saved_networks_dict
 	# If we haven't found a known network, prompt the user to enter the name and password
 	
 	print("Could not find a known network")
-	print("Please enter a network from the list below:")
-	for network in networks:
-		print(network[0].decode())
 
-	network_name = input("WiFi name: ")
+	while network_name == "" or network_name == None:
+		print("Please enter a network from the list below: (enter an empty name to re-scan)")
+		for network in wlan.scan():
+			print(network[0].decode())
+
+		network_name = input("WiFi name: ")
+			
 	network_passwd = input("password: ")
 
 	print("would you like to save these credentials for next time?")
@@ -113,7 +134,13 @@ def prompt_user_network():
 
 def self_test():
 	load_known_networks()
-	setup_wlan()
-	if not scan_networks():
+	setup_wlan()	
+	if scan_networks():
+		# TODO: flash GREEN LED
+		print("FLASH GREEN")
+	else:
+		# TODO: flash RED LED
+		print("FLASH RED")
+	
+	while not connect():
 		prompt_user_network()
-	connect()
