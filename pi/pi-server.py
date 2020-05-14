7f5d84d83c97b8e9d8dc2ca3ad026d02f9914c4e
Isaiah Grace <irgkenya4@gmail.com>
2020-05-14 00:53:10 -0400

    first pass at pi side software, time to debug

--- pi/pi-server.py
+++ pi/pi-server.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
-
-
-#from os.path import getmtime
+# Isaiah Grace
 
 from colorsys import hsv_to_rgb
 from rpi_ws281x import *
@@ -13,16 +11,29 @@ import sys
 import os
 import sched
 import pprint
-#from termcolor import colored    
+from pprint import pprint as pp
+from termcolor import colored    
 
-#import stripDriver
+import stripDriver
+
+
+class FunctionArray():
+    # The whole purpose of this class is so that I can store one of these objects as the signals['fan']['action'],
+    # call that object, and then execute both fan music_effect functions
+    def __init__(self, functions):
+        self.functions = functions
+
+    def __call__(self):
+        for function in functions:
+            function()
+    
 
 class PiServer():
     # LED strip configuration:
     # NOTE: because of the drivers for the PWM module, this script MUST be run with administrator privileges
     #       I don't like it, but the driver needs to call mmap()
     # TODO: Should I use a different DMA channel for each LED strip?
-    strips = {
+    strip_config = {
         'external': {
             'COUNT'      : 150,     # Number of LED pixels. 36 on bottom, 38, on side, 150 total
             'PIN'        : 10,      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
@@ -51,31 +62,61 @@ class PiServer():
             'CHANNEL'    : 1,       # set to '1' for GPIOs 13, 19, 41, 45 or 53
         }
     }
-    
+
+    strips = {
+        'external'   : None,
+        'top_fan'    : None,
+        'bottom_fan' : None
+    }
+        
     signals = {
         'music': {
             'time': 0,
-            'message': None
+            'message': None,
+            'action' : self.nop
         },
         'reading': {
             'time': 0,
-            'message': None
+            'message': None,
+            'action' : self.nop
         },
         'fan': {
             'time': 0,
-            'message': None
+            'message': None,
+            'action' : self.nop
         }
     }
 
     SIGNAL_PATH = '/home/pi/lightRemote/pi/signals/'
     
-    def __init__(self):
-        self.scheduler = sched.scheduler(time.time, time.sleep)
-        #                delay, priority
-        self.scheduler.enter(1, 1, self.read_all_signals)
+    def __init__(self, verbose):
+        self.verbose = verbose
         
+        for strip in self.strips:
+            self.strips[strip] = stripDriver.StripDriver(strip_config[strip])
 
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+
+        # These three variables will let us examine the timimg behaviour of our scheduler
+        self.read_all_signals_time = time()
+        self.signal_actions_time = time()
+        self.refresh_strips_time = time()
+        
+        # Each of these functions schedule themselves after their execution,
+        # so we just need to run them this first time, and they'll keep themselves running.
+        self.read_all_signals()
+        self.signal_actions()
+        self.refresh_strips()
+        
+        if verbose:
+            self.scheduler_status()
+                
+        
     def read_all_signals(self):
+        curr_time = time()
+        self.read_all_signals_delta = curr_time - self.read_all_signals_time
+        self.read_all_signals_time = curr_time
+        
         # for each of the signal names, and their coresponding data dicts:
         for sig, sigData in self.signals.items():
             # Check if the file has been modified since we last looked
@@ -83,6 +124,10 @@ class PiServer():
                 # does mutating a Dict while iterating through it bork the universe?
                 self.signals[sig]['message'] = self.read_signal(sig)
                 self.signals[sig]['time'] = os.path.getmtime(self.SIGNAL_PATH + sig)
+                self.update_action(sig)
+                
+                if self.verbose:
+                    print('Recieved', sig, 'signal')
 
         # The scheduler is not periodic, so we have to re-schedule ourselves if we want to execute again
         self.scheduler.enter(1, 1, self.read_all_signals)
@@ -93,21 +138,87 @@ class PiServer():
             message = json.load(f)
         return message
 
-    
+    def update_action(self, sig):
+        signal = self.signals[sig]
+
+        if sig == 'music':
+            if signal['message']['is_playing']:
+                self.strips['external'].set_base_color_from_valence(signal['message']['valence'])
+            else:
+                self.strips['external'].set_base_color_from_RGB(0,0,0)
+                
+            self.signals[sig]['action'] = self.strips['external'].music_effect
+                
+        if sig == 'reading':
+            pass
+        
+        if sig == 'fan':
+            if signal['message']['fan_on']:
+                if signal['message']['mode'] == 'MUSIC':
+                    if self.signals['music']['message']['is_playing']:
+                        self.strips['top_fan'].set_base_color_from_valence(self.signals['music']['message']['valence'])
+                        self.strips['bottom_fan'].set_base_color_from_valence(self.signals['music']['message']['valence'])
+                    else:
+                        self.strips['top_fan'].set_base_color_from_RGB(0,0,0)
+                        self.strips['bottom_fan'].set_base_color_from_RGB(0,0,0)
+
+                    # See the wizardry above in the class FunctionArray.
+                    # This creates a callable object that will in turn call these music_effect functions
+                    self.signals[sig]['action'] = FunctionArray((self.strips['top_fan'].music_effect,
+                                                                 self.strips.['bottom_fan'].music_effect))
+
+                if signal['message']['mode'] == 'FX':
+                    pass
+                if signal['message']['mode'] == 'NOTIFY':
+                    pass
+                
+            else:
+                self.strips['top_fan'].clear()
+                self.strips['bottom_fan'].clear()
+                self.signals[sig]['action'] = self.nop
+        
+        
+    def signal_actions(self):
+        curr_time = time()
+        self.signal_actions_delta = curr_time - self.signal_actions_time
+        self.signal_actions_time = curr_time
+        
+        for signal in self.signals.items():
+            signal['action']()
+
+        self.scheduler.enter(0.1, 2, self.signal_actions)
+        
+    def refresh_strips(self):
+        curr_time = time()
+        self.refresh_strips_delta = curr_time - self.refresh_strips_time
+        self.refresh_strips_time = curr_time
+
+        for strip in self.strips.values():
+            strip.refresh()
+            
+        self.scheduler.enter(0.1, 3, self.refresh_strips)
+        
+        
     def scheduler_status(self):
         # print out some status information
         print(self.scheduler.queue)
-        #pprint.pprint(self.signals)
+        print("read_all_signals :",self.read_all_signals_delta)
+        print("signal_actions   :",self.signal_actions_delta)
+        print("refresh_strips   :",self.refresh_strips_delta)
+
+        self.scheduler.enter(2, 1, self.scheduler_status)
         
-        
-    
+
+    def nop(self):
+        # This is needed so that signal actions can always be callable even if there is nothing to do
+        pass    
     
         
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == '-q':
         sys.stdout = open(os.devnull, 'w')
 
-    server = PiServer()
+    server = PiServer(verbose=True)
 
     # The scheduler is blocking, so this is our infinite loop:
     server.scheduler.run()
---
+++ pi/stripDriver.py
@@ -0,0 +1,140 @@
+# Isaiah Grace
+# May 13 2020
+
+import colorsys
+import random
+import rpi_ws281x
+
+
+class StripDriver():
+    def __init__(self, config):
+        self.strip = rpi_ws281x.PixelStrip(
+            config['COUNT'],
+            config['PIN'],
+            config['FREQ_HZ'],
+            config['DMA'],
+            config['INVERT'],
+            config['BRIGHTNESS'],
+            config['CHANNEL']
+        )
+        self.strip.begin()
+
+        # This is the color that pixels will gravitate towards
+        self.base_red = 0 # range 0 - 255
+        self.base_blue = 0 # range 0 - 255
+        self.base_green = 0 # range 0 - 255
+
+        # Pixels can mutate to any color with +- the tolerance from the base
+        # A tolerance of 0 means all pixels will be the base_color
+        # A tolerance of 255 means pixels color will randomly span the entire color space
+        self.tolerance = 0         # range 0 - 255
+
+        # This is the % chance that a color value of a pixel has to mutate
+        self.mutation_rate = 0     # range 0 - 100
+
+        # A mutated color will change by this value, either + or -
+        # increasing this will increase the speed that the colors change
+        self.mutation_step = 2     # range 0 - 255
+
+        # This offset is applied to the hue color wheel and will create a phase change from the valence mapping to the RGB mapping.
+        # This website has a good demonstration of HSV color: http://aleto.ch/color-mixer/
+        # A offset value of 0.66 would be equivelent of a Hue value of 240 on the above site.
+        # Our mapping of the range 0 - 1 to the RGB color space is then represented by the circumpfrence of the HSV color wheel.
+        # Note that this only allows mixing of two of the three colors, one will always be zero.
+        # Thankfully our mutations will take care of this and we can rely on them to produce more colors.
+        self.hue_offset = 0.66     # range 0 - 1
+
+        # These HSV values allow a direct mapping from the 0 - 1 range to the RGB space
+        # See: https://en.wikipedia.org/wiki/HSL_and_HSV
+        # maximum values for S and V create more vivid and intense colors, and are a good choice with these cheep LEDs
+        self.hsv_S = 1
+        self.hsv_V = 1
+        
+    def refresh(self):
+        # This transmits data to the LED strip to set the color of each LED according to the strip array
+        strip.show()
+
+        
+    def clear(self):
+        # DOES NOT TRANSMIT NEW COLORS TO STRIP
+        # refresh is needed after this to actually turn off the lights
+        for i in range(strip.numPixels()):
+            strip.setPixelColorRGB(i,0,0,0)
+
+                
+    def set_base_color_from_valence(self, valence):
+        # Takes a valence value (0 - 1) describing the song and returns a color mapping of that value, with the hue_offset applied
+        # The offset shifts 
+        hue = valence + self.hue_offset
+        if hue > 1:
+            hue = hue - 1
+            
+        # colorsys.hsv_to_rgb returns values from 0 - 1, we need to scale these to integers in the range 0 - 255
+        (self.base_red, self.base_green, self.base_blue) = tuple(int(i * 255) for i in colorsys.hsv_to_rgb(hue, self.hsv_S, self.hsv_V))
+
+
+    def set_base_color_from_RGB(self, red, green, blue):
+        self.base_red = red
+        self.base_green = green
+        self.base_blue = blue
+    
+    def music_effect(self):
+        for i in range(self.strip.numPixels()):
+            pixel_color = self.strip.getPixelColor(i)
+            
+            # Extract the R G B values for this pixel from the packed 24 bit color
+            pixel_color = ((0xFF0000 & pixel_color) >> 16,
+                           (0x00FF00 & pixel_color) >> 8,
+                           (0x0000FF & pixel_color))
+            
+            # Mutate each of the colors to find a new color value for the pixel
+            pixel_color = mutate_color(pixel_color, self.mutation_rate, self.mutation_step)
+            
+            # Apply the limiting bound on the new pixel color
+            red   = apply_bound(pixel_color[0], self.base_red,   self.tolerance)
+            green = apply_bound(pixel_color[1], self.base_green, self.tolerance)
+            blue  = apply_bound(pixel_color[2], self.base_blue,  self.tolerance)
+            
+            # Update the color of this pixel
+            self.strip.setPixelColorRGB(i, red, green, blue)
+            
+            
+    def mutate_color(self, color, mutation_rate, mutationStep):
+        # Applies a random mutation to each pixel, allowing a natural variation between pixel colors
+        mutated_color = [0,0,0]
+        
+        # This arithmetic means we only need one call to random in order to generate all three random numbers
+        rand = random.randint(0,10000000)
+        mutation = [rand % 100,
+                    (rand // 100) % 100,
+                    (rand // 100000) % 100
+        ]
+
+        for i in (0,1,2):
+            if mutation[i] < mutation_rate:
+                # Apply the color mutation, either increase or decrease the color
+                if mutation[i] % 2 == 0:
+                    color = color + mutationStep
+                else:
+                    color = color - mutationStep
+
+                # Make sure the mutated color is within 0 - 255
+                if color > 255:
+                    color = 255
+                if color < 0:
+                    color = 0
+
+                mutated_color[i] = color
+            
+        return color
+
+    
+    def apply_bound(self.color, base_color, bound_size):
+        # Applies the bound on a particular color value (0 - 255).
+        # If the color is outside the allowed bound, gently push it towards the bound.
+        # This allows the lights to turn on/off change colors from song to song gradually
+        if color > base_color + bound_size:
+            return color - 1
+        if color < base_color - bound_size:
+            return color + 1
+        return color
