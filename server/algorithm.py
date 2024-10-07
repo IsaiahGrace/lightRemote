92a29a05905dbeeeeb352896dfa36c3bc09f98db
Isaiah Grace <irgkenya4@gmail.com>
2024-10-07 17:23:00 -0400

    Moved the pixel algorithm into a separate file so it can be re-used by different backends.

--- server/algorithm.py
+++ server/algorithm.py
@@ -1,7 +1,70 @@
+import math
+import random
+
+
 class Algorithm:
     def __init__(self, config, params):
         self.params = params
         self.config = config
 
+        self.hue_vel = [random.random() for _ in range(self.config.num_pixels)]
+        self.linear_sat_vel = [0] * self.config.num_pixels
+        self.linear_val_vel = [0] * self.config.num_pixels
+
+        self.linear_sat = [0] * self.config.num_pixels
+        self.linear_val = [0] * self.config.num_pixels
+
+        self.hue = [0] * self.config.num_pixels
+        self.sat = [0] * self.config.num_pixels
+        self.val = [0] * self.config.num_pixels
+
+    def set_params(self, params):
+        self.params = params
+        self.params.s = math.pow(self.params.s, self.config.scaling_root)
+        self.params.v = math.pow(self.params.v, self.config.scaling_root)
+
     def update(self):
-        pass
+        for position, velocity, target, tolerance, wraps in (
+            (self.hue, self.hue_vel, self.params.h, self.params.dh, True),
+            (self.linear_sat, self.linear_sat_vel, self.params.s, self.params.ds, False),
+            (self.linear_val, self.linear_val_vel, self.params.v, self.params.dv, False),
+        ):
+            inverse_target = (target + 0.5) % 1
+            for i in range(len(position)):
+                p = position[i]
+                v = velocity[i]
+
+                if random.random() < self.params.t:
+                    if random.random() > 0.5:
+                        v += self.config.delta
+                    else:
+                        v -= self.config.delta
+
+                # Sometimes the fastest way to get to the target is to decriment
+                # the hue, and wrap around from 0 -> 1. The "inverse target" is
+                # the opposite point on the hue circle. By adding or subtracting
+                # 1, we make the comparisons below simpler. Because we wrap the
+                # values at the end of this function, the offset here doesn't
+                # effect the final value.
+                if wraps:
+                    if inverse_target > target and p > inverse_target:
+                        p -= 1
+                    elif inverse_target < target and p < inverse_target:
+                        p += 1
+
+                if (wraps or target == 0.0) and p > target + tolerance:
+                    v -= self.config.delta * 0.1
+                if p < target - tolerance:
+                    v += self.config.delta * 0.1
+
+                p += v
+                v *= 0.85  # friction, sensitive
+
+                if wraps:
+                    p %= 1
+                else:
+                    p = max(p, 0.0)
+                    p = min(p, 1.0)
+
+                position[i] = p
+                velocity[i] = v
--- server/debug.py
+++ server/debug.py
@@ -1,3 +1,4 @@
+import algorithm
 import colorsys
 import math
 import params
@@ -17,20 +18,9 @@ config = params.Config(
 
 class Display:
     def __init__(self):
-        self.params = params.Params()
         self.param_updates = queue.Queue()
         self.frame = 0
-
-        self.hue_vel = [random.random() for _ in range(config.num_pixels)]
-        self.linear_sat_vel = [0] * config.num_pixels
-        self.linear_val_vel = [0] * config.num_pixels
-
-        self.linear_sat = [0] * config.num_pixels
-        self.linear_val = [0] * config.num_pixels
-
-        self.hue = [0] * config.num_pixels
-        self.sat = [0] * config.num_pixels
-        self.val = [0] * config.num_pixels
+        self.algorithm = algorithm.Algorithm(config, params.Params())
 
         self.red = [0] * config.num_pixels
         self.green = [0] * config.num_pixels
@@ -59,7 +49,7 @@ class Display:
         while not rl.window_should_close():
             self.frame += 1
             self.update_params()
-            self.update_pixels()
+            self.algorithm.update()
             self.update_rgb()
             self.render()
         rl.unload_render_texture(self.texture_3d)
@@ -67,67 +57,20 @@ class Display:
 
     def update_params(self):
         try:
-            self.params = self.param_updates.get(block=False)
-            self.params.s = math.pow(self.params.s, config.scaling_root)
-            self.params.v = math.pow(self.params.v, config.scaling_root)
+            params = self.param_updates.get(block=False)
+            self.algorithm.set_params(params)
         except queue.Empty:
             pass
 
-    def update_pixels(self):
-        for position, velocity, target, tolerance, wraps in (
-            (self.hue, self.hue_vel, self.params.h, self.params.dh, True),
-            (self.linear_sat, self.linear_sat_vel, self.params.s, self.params.ds, False),
-            (self.linear_val, self.linear_val_vel, self.params.v, self.params.dv, False),
-        ):
-            inverse_target = (target + 0.5) % 1
-            for i in range(len(position)):
-                p = position[i]
-                v = velocity[i]
-
-                if random.random() < self.params.t:
-                    if random.random() > 0.5:
-                        v += config.delta
-                    else:
-                        v -= config.delta
-
-                # Sometimes the fastest way to get to the target is to decriment
-                # the hue, and wrap around from 0 -> 1. The "inverse target" is
-                # the opposite point on the hue circle. By adding or subtracting
-                # 1, we make the comparisons below simpler. Because we wrap the
-                # values at the end of this function, the offset here doesn't
-                # effect the final value.
-                if wraps:
-                    if inverse_target > target and p > inverse_target:
-                        p -= 1
-                    elif inverse_target < target and p < inverse_target:
-                        p += 1
-
-                if (wraps or target == 0.0) and p > target + tolerance:
-                    v -= config.delta * 0.1
-                if p < target - tolerance:
-                    v += config.delta * 0.1
-
-                p += v
-                v *= 0.85  # friction, sensitive
-
-                if wraps:
-                    p %= 1
-                else:
-                    p = max(p, 0.0)
-                    p = min(p, 1.0)
-
-                position[i] = p
-                velocity[i] = v
-
     def update_rgb(self):
         for linear, tuned in (
-            (self.linear_sat, self.sat),
-            (self.linear_val, self.val),
+            (self.algorithm.linear_sat, self.algorithm.sat),
+            (self.algorithm.linear_val, self.algorithm.val),
         ):
             for i, l in enumerate(linear):
                 tuned[i] = math.pow(l, 1.0 / config.scaling_root)
 
-        for i, (h, s, v) in enumerate(zip(self.hue, self.sat, self.val)):
+        for i, (h, s, v) in enumerate(zip(self.algorithm.hue, self.algorithm.sat, self.algorithm.val)):
             r, g, b = colorsys.hsv_to_rgb(h, s, v)
             self.red[i] = int(r * 255)
             self.green[i] = int(g * 255)
@@ -143,14 +86,15 @@ class Display:
         self.draw_vs_plot()
         self.draw_h_line()
 
-        rl.draw_text(f"Hue\n\n{self.params.h:0.4f}", 5, 5, 30, rl.DARKGRAY)
+        rl.draw_text(f"Hue\n\n{self.algorithm.params.h:0.4f}", 5, 5, 30, rl.DARKGRAY)
         rl.draw_rectangle_v([window_size // 2 - 5, 0], [10, window_size], rl.LIGHTGRAY)
         rl.draw_rectangle_v([0, window_size // 2 - 5], [window_size, 10], rl.LIGHTGRAY)
 
         rl.end_drawing()
 
     def draw_h_line(self):
-        for r, g, b, h in zip(self.red, self.green, self.blue, self.hue):
+        params = self.algorithm.params
+        for r, g, b, h in zip(self.red, self.green, self.blue, self.algorithm.hue):
             rl.draw_circle(540 + int(h * 440), 900, 6, [r, g, b, 255])
 
         # main axis, ticks at 0 & 1
@@ -161,59 +105,65 @@ class Display:
         rl.draw_text("1", 540 + 430, 940, 30, rl.DARKGRAY)
 
         # Hue target value
-        rl.draw_line_v([540 + int(440 * self.params.h), 910], [540 + int(440 * self.params.h), 930], rl.DARKGRAY)
-        rl.draw_text(f"{self.params.h:0.4f}", 500 + int(440 * self.params.h), 940, 30, rl.DARKGRAY)
+        rl.draw_line_v(
+            [540 + int(440 * params.h), 910],
+            [540 + int(440 * params.h), 930],
+            rl.DARKGRAY,
+        )
+        rl.draw_text(f"{params.h:0.4f}", 500 + int(440 * params.h), 940, 30, rl.DARKGRAY)
 
         # Hue bounds:
         rl.draw_line_v(
-            [540 + int(440 * ((self.params.h - self.params.dh) % 1)), 910],
-            [540 + int(440 * ((self.params.h - self.params.dh) % 1)), 930],
+            [540 + int(440 * ((params.h - params.dh) % 1)), 910],
+            [540 + int(440 * ((params.h - params.dh) % 1)), 930],
             rl.DARKGRAY,
         )
         rl.draw_line_v(
-            [540 + int(440 * ((self.params.h + self.params.dh) % 1)), 910],
-            [540 + int(440 * ((self.params.h + self.params.dh) % 1)), 930],
+            [540 + int(440 * ((params.h + params.dh) % 1)), 910],
+            [540 + int(440 * ((params.h + params.dh) % 1)), 930],
             rl.DARKGRAY,
         )
 
         # Opposite Hue
         rl.draw_line_v(
-            [540 + int(440 * ((self.params.h + 0.5) % 1)), 890],
-            [540 + int(440 * ((self.params.h + 0.5) % 1)), 930],
+            [540 + int(440 * ((params.h + 0.5) % 1)), 890],
+            [540 + int(440 * ((params.h + 0.5) % 1)), 930],
             rl.BLACK,
         )
 
     def draw_vs_plot(self):
-        s = math.pow(self.params.s, 1.0 / config.scaling_root)
-        v = math.pow(self.params.v, 1.0 / config.scaling_root)
+        params = self.algorithm.params
+        s = math.pow(params.s, 1.0 / config.scaling_root)
+        v = math.pow(params.v, 1.0 / config.scaling_root)
         x = 500 + int(s * 500)
         y = int(500 - v * 500)
         rl.draw_line_v([x, 0], [x, 500], rl.DARKGRAY)
         rl.draw_line_v([500, y], [1000, y], rl.DARKGRAY)
 
-        for r, g, b, s, v in zip(self.red, self.green, self.blue, self.sat, self.val):
+        for r, g, b, s, v in zip(self.red, self.green, self.blue, self.algorithm.sat, self.algorithm.val):
             x = 500 + int(s * 500)
             y = int(500 - v * 500)
             rl.draw_circle(x, y, 5, [r, g, b, 255])
 
     def draw_hs_plot(self):
-        x = 250 + int(245 * math.cos(self.params.h * 2 * math.pi))
-        y = 250 + int(245 * math.sin(self.params.h * 2 * math.pi))
+        params = self.algorithm.params
+        x = 250 + int(245 * math.cos(params.h * 2 * math.pi))
+        y = 250 + int(245 * math.sin(params.h * 2 * math.pi))
         rl.draw_line_v([250, 250], [x, y], rl.DARKGRAY)
 
-        x = 250 + int(245 * math.cos((self.params.h - self.params.dh) * 2 * math.pi))
-        y = 250 + int(245 * math.sin((self.params.h - self.params.dh) * 2 * math.pi))
+        x = 250 + int(245 * math.cos((params.h - params.dh) * 2 * math.pi))
+        y = 250 + int(245 * math.sin((params.h - params.dh) * 2 * math.pi))
         rl.draw_line_v([250, 250], [x, y], rl.LIGHTGRAY)
 
-        x = 250 + int(245 * math.cos((self.params.h + self.params.dh) * 2 * math.pi))
-        y = 250 + int(245 * math.sin((self.params.h + self.params.dh) * 2 * math.pi))
+        x = 250 + int(245 * math.cos((params.h + params.dh) * 2 * math.pi))
+        y = 250 + int(245 * math.sin((params.h + params.dh) * 2 * math.pi))
         rl.draw_line_v([250, 250], [x, y], rl.LIGHTGRAY)
 
-        s = math.pow(self.params.s, 1.0 / config.scaling_root)
+        s = math.pow(params.s, 1.0 / config.scaling_root)
         rl.draw_circle_lines(250, 250, 240 * s, rl.LIGHTGRAY)
         rl.draw_circle_lines(250, 250, 240, rl.LIGHTGRAY)
 
-        for r, g, b, h, s in zip(self.red, self.green, self.blue, self.hue, self.sat):
+        for r, g, b, h, s in zip(self.red, self.green, self.blue, self.algorithm.hue, self.algorithm.sat):
             x = 250 + int(240 * s * math.cos(h * 2 * math.pi))
             y = 250 + int(240 * s * math.sin(h * 2 * math.pi))
             rl.draw_circle(x, y, 5, [r, g, b, 255])
@@ -233,7 +183,14 @@ class Display:
 
         rl.draw_cylinder_wires([0, -1, 0], 1, 1, 1, 16, rl.LIGHTGRAY)
 
-        for r, g, b, h, s, v in zip(self.red, self.green, self.blue, self.hue, self.sat, self.val):
+        for r, g, b, h, s, v in zip(
+            self.red,
+            self.green,
+            self.blue,
+            self.algorithm.hue,
+            self.algorithm.sat,
+            self.algorithm.val,
+        ):
             rl.draw_cube_v(
                 [s * math.cos(h * 2 * math.pi), -v, s * math.sin(h * 2 * math.pi)],
                 [0.05, 0.05, 0.05],
