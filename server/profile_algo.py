8644b52f11bd4b488211a7b672a0742c2795bc24
Isaiah Grace <irgkenya4@gmail.com>
2024-10-07 17:24:33 -0400

    tweaked the timing script.

--- server/profile_algo.py
+++ server/profile_algo.py
@@ -1,19 +1,30 @@
+from rich import print
 import algorithm
 import cProfile
 import params
+import timeit
 
 config = params.Config(
     delta=(1.0 / 2000.0),
-    num_pixels=4000,
+    num_pixels=13398,
     scaling_root=2.0,
 )
 
-algo = algorithm.Algorithm(config, params.Params())
+params = params.Params(
+    h=1.0,
+    s=0.5,
+    v=0.8,
+    dh=0.05,
+    ds=0.0,
+    dv=0.0,
+    t=0.5,
+)
 
 
-def main():
-    cProfile.run("algo.update()")
+algo = algorithm.Algorithm(config, params)
 
+# cProfile.run("algo.update()")
 
-if __name__ == "__main__":
-    main()
+repeat = 10
+time = timeit.timeit(algo.update, number=repeat)
+print(f"{time} / {repeat} = {time/repeat} seconds per update")
