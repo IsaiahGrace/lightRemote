8f5ac4171a889477b8bc207d0262364d93f9afb3
Isaiah Grace <isaiah@graces.com>
2025-11-17 20:35:13 -0500

    Edited the source code to switch between the red and green LEDs

--- src/main.c
+++ src/main.c
@@ -3,40 +3,49 @@
 
 // Settings
 static const int32_t sleep_time_ms = 500;
-static const struct gpio_dt_spec led =
-    GPIO_DT_SPEC_GET(DT_ALIAS(my_led), gpios);
+static const struct gpio_dt_spec red_led = GPIO_DT_SPEC_GET(DT_ALIAS(my_red_led), gpios);
+static const struct gpio_dt_spec green_led = GPIO_DT_SPEC_GET(DT_ALIAS(my_green_led), gpios);
 
-int main(void) {
-  int ret;
-  int state = 0;
+int main(void)
+{
+    int ret;
+    int state = 0;
 
-  // Make sure that the GPIO was initialized
-  if (!gpio_is_ready_dt(&led)) {
-    return 0;
-  }
-
-  // Set the GPIO as output
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-  if (ret < 0) {
-    return 0;
-  }
-
-  // Do forever
-  while (1) {
-
-    // Change the state of the pin and print
-    state = !state;
-    printk("LED state: %d\r\n", state);
-
-    // Set pin state
-    ret = gpio_pin_set_dt(&led, state);
-    if (ret < 0) {
-      return 0;
+    // Make sure that the GPIO was initialized
+    if (!gpio_is_ready_dt(&red_led) || !gpio_is_ready_dt(&green_led)) {
+        return 0;
     }
 
-    // Sleep
-    k_msleep(sleep_time_ms);
-  }
+    // Set the GPIO as output
+    ret = gpio_pin_configure_dt(&red_led, GPIO_OUTPUT);
+    if (ret < 0) {
+        return 0;
+    }
 
-  return 0;
+    ret = gpio_pin_configure_dt(&green_led, GPIO_OUTPUT);
+    if (ret < 0) {
+        return 0;
+    }
+
+    // Do forever
+    while (1) {
+        // Change the state of the pin and print
+        state = !state;
+        printk("LED state: %d\r\n", state);
+
+        // Set pin state
+        ret = gpio_pin_set_dt(&red_led, state);
+        if (ret < 0) {
+            return 0;
+        }
+        ret = gpio_pin_set_dt(&green_led, !state);
+        if (ret < 0) {
+            return 0;
+        }
+
+        // Sleep
+        k_msleep(sleep_time_ms);
+    }
+
+    return 0;
 }
